87f23a80b700956ed430fb6c532339dc6f021771
James Freeman <freeman.james.h@gmail.com>
2016-09-22 11:22:34 -0600

    dolluh signs

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
-var cookieSession = require('cookie-session')
+//needs cookie session
 
 var routes = require('./routes/index');
 
@@ -18,10 +18,7 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(cookieSession({
-  name: 'authorizationtest',
-  secret: 'secret'
-}))
+//needs cookie-session
 
 app.use(express.static(path.join(__dirname, 'public')));
 
